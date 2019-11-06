c9cf12e101421dbf544f17212843094ac8f5b0cc
weigang <1337116153@qq.com>
2019-11-06 17:58:12 +0800

    模板初试

--- 11-模板引擎.go
+++ 11-模板引擎.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func process(w http.ResponseWriter, r *http.Request) {
+func tmpl(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("11-tmpl.html")
 	if t != nil {
 		t.Execute(w, "Hello World!")
@@ -19,6 +19,6 @@ func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/process", process)
+	http.HandleFunc("/process", tmpl)
 	server.ListenAndServe()
 }
