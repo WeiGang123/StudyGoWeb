3081c7fc543e9f4616d9d8596f61b8a7b0fa8c58
weigang <1337116153@qq.com>
2019-11-07 20:11:41 +0800

    条件动作

---
+++ 12-条件动作.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func condition(w http.ResponseWriter, r *http.Request) {
+	// 首先进行语法分析
+	t, _ := template.ParseFiles("template/12-conditionAction.html")
+	rand.Seed(time.Now().Unix()) // 随机数种子
+	// 将数据应用到模板里
+	t.Execute(w, rand.Intn(10) > 5)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/conditionAction", condition)
+
+	server.ListenAndServe()
+}
