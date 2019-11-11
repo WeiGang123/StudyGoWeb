b15f7407c5d1770e95a31d3ab56c67ad767093b8
weigang <1337116153@qq.com>
2019-11-11 20:18:21 +0800

    服务端向客户端发送相应

--- 08-ResponseWriter.go
+++ 08-ResponseWriter.go
@@ -7,8 +7,8 @@ import (
 )
 
 // 用来序列化json
-type Post struct {
-	User string
+type Post1 struct {
+	User    string
 	Threads []string
 }
 
@@ -19,7 +19,7 @@ type Post struct {
 //	 如果用户在调用Write方法之前没有执行WriteHeader，那么程序会默认将200 OK 作为响应的状态码
 // - Header 调用此方法可以获得一个由首部组成的映射，修改这个映射就可以修改首部，修改后的首部被包含在HTTP响应里面，发送至客户端
 
-func writeExample(w http.ResponseWriter, r *http.Request)  {
+func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
 <head><title>Go Web Programming</title></head>
 <body><h1>Hello World</h1></body>
@@ -27,12 +27,12 @@ func writeExample(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte(str))
 }
 
-func writeHeaderExample(w http.ResponseWriter, r *http.Request)  {
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(501)
 	fmt.Fprintln(w, "No such service, try next door")
 }
 
-func headerExample(w http.ResponseWriter, r *http.Request)  {
+func headerExample(w http.ResponseWriter, r *http.Request) {
 	// 对HTTP响应添加Location的首部字段进行HTTP的重定向
 	w.Header().Set("Location", "http://www.weigang.wang")
 	// WriteHeader方法在执行完毕以后就不允许再对首部进行写入了，
@@ -41,17 +41,17 @@ func headerExample(w http.ResponseWriter, r *http.Request)  {
 }
 
 // 向客户端直接返回json数据
-func jsonExample(w http.ResponseWriter, r *http.Request)  {
+func jsonExample(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	post := &Post{
-		User: "wang weigang",
+	post := &Post1{
+		User:    "wang weigang",
 		Threads: []string{"first", "second", "third"},
 	}
 	json, _ := json2.Marshal(post)
 	w.Write(json)
 }
 
-func  main()  {
+func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
