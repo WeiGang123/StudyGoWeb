3a8929755baae13108977e0ed92ffdde59b8f5eb
weigang <1337116153@qq.com>
2019-11-14 22:10:37 +0800

    使用GO对Postgres执行CRUD操作

--- 21-go连接数据库.go
+++ 21-go连接数据库.go
@@ -50,20 +50,30 @@ func posts(limit int) (posts1 []post, err error) {
 	return
 }
 
+// 通过向函数传递id来返回一个包含完整的数据的变量
 func getPost(id int) (myPost post, err error) {
+	// 先创建一个空结构体用来存储数据
 	myPost = post{}
+	// 利用QueryRow进行查询并使用Scan来进行数据的写入。
+	// 我们直接使用sql.DB的QueryRow而没有使用sql.Stmt的 是因为我们无需重复的执行sql语句
+	// 其实两种都可以使用，这里只是展示了另一种写法。
 	err = Db.QueryRow("select id, content, author from posts where id"+
 		" = $1", id).Scan(&myPost.Id, &myPost.Content, &myPost.Author)
 	return
 }
 
 func (myPost *post) Create() (err error) {
+	// 定义一条SQL预处理语句，这就是一个SQL语句模板，在执行时需要给语句中的参数提供具体值
 	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	// Prepare创建预处理语句
 	stmt, err := Db.Prepare(statement)
 	if err != nil {
 		return
 	}
 	defer stmt.Close()
+	// QueryRow方法用来调用预处理语句，并把来自接收者(mypost)的数据传递给该方法
+	// 如果被执行的sql语句返回多于一个sql.Row，这个方法只能返回第一个sql.Row，并丢掉其他的
+	// Scan会将查询到的行中的值复制到程序为其提供的参数里面，在这里会把id列的值设置为接收者的值
 	err = stmt.QueryRow(myPost.Content, myPost.Author).Scan(&myPost.Id)
 	return
 }
